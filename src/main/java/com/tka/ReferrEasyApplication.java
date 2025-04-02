6e13c724b16959fa34a673d5f3a52e0ad7af8aef
Bhargav <vyasb1503@gmail.com>
2025-04-02 23:42:00 +0530

    adding the application  and the dependencyes

---
+++ src/main/java/com/tka/ReferrEasyApplication.java
@@ -0,0 +1,13 @@
+package com.tka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferrEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferrEasyApplication.class, args);
+	}
+
+}
