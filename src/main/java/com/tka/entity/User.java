0254611d664ccd0f8b465405547eb1e4ac2dbb4c
Bhargav <vyasb1503@gmail.com>
2025-04-03 23:14:06 +0530

    this is the commit for the Userentity

--- src/main/java/com/tka/entity/User.java
+++ src/main/java/com/tka/entity/User.java
@@ -1,16 +1,24 @@
 package com.tka.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+import lombok.Data;
+@Data
 @Entity
 @Table(name ="users")
+
 public class User {
 	 @Id
 	 @GeneratedValue(strategy = GenerationType.IDENTITY)
 	 private Long id;
+	 private String username;
+	  private String password;
+	  private String role;
+	  private List<Referral> Referals;
 
 }
