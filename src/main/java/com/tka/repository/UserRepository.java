f9e15c31ad619beeac5f46ba64c1d8cef931aaef
Bhargav <vyasb1503@gmail.com>
2025-04-03 23:27:30 +0530

    implementing the repository and entends itr with the jpa repository

---
+++ src/main/java/com/tka/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tka.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tka.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
