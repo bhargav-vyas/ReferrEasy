9030e4251b8e7981b8336925410a55652ec17799
Bhargav <vyasb1503@gmail.com>
2025-04-05 23:35:50 +0530

    this is the implemantation for the  interface

--- src/main/java/com/tka/service/UserService.java
+++ src/main/java/com/tka/service/UserService.java
@@ -1,11 +1,20 @@
 package com.tka.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.tka.entity.User;
 import com.tka.repository.UserRepository;
 
 public interface UserService {
+	
+
+	void resisterUser(User user);
+	boolean loginUser(String username, String pasword);
+	List<User> getAllUsers();
+	User getUserById(Long id);
+	void deletUserById(Long id);
 	
 
 }
