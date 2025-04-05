f33ed35d657252aed7f4161f13f6d5e7ce663225
Bhargav <vyasb1503@gmail.com>
2025-04-05 23:06:22 +0530

    this is the implemantation for the  interface userservice

--- src/main/java/com/tka/service/UserService.java
+++ src/main/java/com/tka/service/UserService.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.tka.entity.User;
 import com.tka.repository.UserRepository;
 
-public interface UserServi {
+public interface UserService {
 	
 
 }
