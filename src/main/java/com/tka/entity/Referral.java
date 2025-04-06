a69e82484732ca1d71ffd8a548985179a8af7723
Bhargav <vyasb1503@gmail.com>
2025-04-06 23:34:36 +0530

    this is  the implementation of the referal entity

--- src/main/java/com/tka/entity/Referral.java
+++ src/main/java/com/tka/entity/Referral.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -12,4 +14,8 @@ public class Referral {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+	@ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
