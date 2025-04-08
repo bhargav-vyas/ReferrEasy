970854131ccff4d497b4a7afb8da2fee9a25b934
Bhargav <vyasb1503@gmail.com>
2025-04-08 19:46:18 +0530

    this is the  login  user

--- src/main/java/com/tka/entity/User.java
+++ src/main/java/com/tka/entity/User.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -21,6 +22,7 @@ public class User {
 	 @Id
 	 @GeneratedValue(strategy = GenerationType.IDENTITY)
 	 private Long id;
+	 @Column(unique = true)
 	 private String username;
 	  private String password;
 	  private String role;
