2c6a3340e48e1bb2a52ef199f3514b756923c2b9
Bhargav <vyasb1503@gmail.com>
2025-04-11 21:44:59 +0530

    this is the job controller and create api  the job controller using the job entity

--- src/main/java/com/tka/entity/job.java
+++ src/main/java/com/tka/entity/job.java
@@ -4,15 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name ="Job")
 public class Job {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 		private Long id;
-	
 		private String title;
 		private String company;
 		private String description;
