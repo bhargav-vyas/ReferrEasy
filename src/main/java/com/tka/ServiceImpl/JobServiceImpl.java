1927638192fbcd4a1b5451fd44fad39cc88264f2
Bhargav <vyasb1503@gmail.com>
2025-04-11 21:40:54 +0530

    this is the job controller and create api  the job controller using the job entity

--- src/main/java/com/tka/ServiceImpl/JobServiceImpl.java
+++ src/main/java/com/tka/ServiceImpl/JobServiceImpl.java
@@ -1,10 +1,11 @@
 package com.tka.ServiceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.tka.entity.Job;
 import com.tka.repository.Jobrepository;
-
+@Service
 public class JobServiceImpl {
 @Autowired  
 private Jobrepository jobrepository;
