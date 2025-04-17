eeb1216391fd5f87070f5247a513128e5c667d5c
Bhargav <vyasb1503@gmail.com>
2025-04-17 22:48:21 +0530

    this is the referal  repository

--- src/main/java/com/tka/repository/Refferalrepositopryt.java
+++ src/main/java/com/tka/repository/Refferalrepositopryt.java
@@ -1,5 +1,10 @@
 package com.tka.repository;
 
-public class Refferalrepositopryt {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tka.entity.Referral;
+
+public interface Refferalrepositopryt extends  JpaRepository<Referral,Long>{
+	
 
 }
