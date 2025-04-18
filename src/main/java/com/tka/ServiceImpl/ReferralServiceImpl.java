8f1e5680e634a586e35f03c4cd89e159e27d9a2e
Bhargav <vyasb1503@gmail.com>
2025-04-18 22:57:26 +0530

    this is the  referral controller

--- src/main/java/com/tka/ServiceImpl/ReferralServiceImpl.java
+++ src/main/java/com/tka/ServiceImpl/ReferralServiceImpl.java
@@ -1,5 +1,5 @@
 package com.tka.ServiceImpl;
 
 public class ReferralServiceImpl {
-
+	
 }
--- src/main/java/com/tka/service/ReferralService.java
+++ src/main/java/com/tka/service/ReferralService.java
@@ -1,5 +1,13 @@
 package com.tka.service;
 
+import java.util.List;
+
+import com.tka.entity.Referral;
+
 public interface ReferralService {
+	Referral createReferral(Referral referral);
+	List<Referral> getallReferals();
+	Referral getReferralById(Long id);
+
 
 }
