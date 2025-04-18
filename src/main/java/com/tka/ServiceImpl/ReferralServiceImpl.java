4b5fac3a2cf2472da7bc77edad79255312c3e819
Bhargav <vyasb1503@gmail.com>
2025-04-18 23:05:04 +0530

    this is the  implemenmtation of the  create referral in the controller

--- src/main/java/com/tka/ServiceImpl/ReferralServiceImpl.java
+++ src/main/java/com/tka/ServiceImpl/ReferralServiceImpl.java
@@ -1,5 +1,15 @@
 package com.tka.ServiceImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tka.entity.Referral;
+import com.tka.repository.Referalrepositopryt;
+
 public class ReferralServiceImpl {
+	@Autowired Referalrepositopryt referalrepositopryt;
+
+	public Referral  createReferral(Referral referral) {
+		return referalrepositopryt.save(referral);
+	}
 	
 }
--- src/main/java/com/tka/controller/Referralcontroller.java
+++ src/main/java/com/tka/controller/Referralcontroller.java
@@ -1,15 +1,24 @@
 package com.tka.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tka.ServiceImpl.ReferralServiceImpl;
+import com.tka.entity.Referral;
 
 @RestController
 @RequestMapping("/api/referrals")
 public class Referralcontroller {	
 	@Autowired
 	private ReferralServiceImpl referralServiceImpl;
+	
+	@PostMapping
+	public String   createReferral(@RequestBody Referral referral) {
+		 referralServiceImpl.createReferral(referral);
+		 return "the referral is created";
+	}
 }
   
