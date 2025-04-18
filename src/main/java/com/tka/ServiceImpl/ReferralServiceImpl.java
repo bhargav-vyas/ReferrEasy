6d5e9562c356876050644fb03009110b59487ba2
Bhargav <vyasb1503@gmail.com>
2025-04-18 23:10:46 +0530

    this is the  implemenmtation of the  create referral in the controller

--- src/main/java/com/tka/ServiceImpl/ReferralServiceImpl.java
+++ src/main/java/com/tka/ServiceImpl/ReferralServiceImpl.java
@@ -1,10 +1,11 @@
 package com.tka.ServiceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.tka.entity.Referral;
 import com.tka.repository.Referalrepositopryt;
-
+@Service
 public class ReferralServiceImpl {
 	@Autowired Referalrepositopryt referalrepositopryt;
 
