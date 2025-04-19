dbfb72401d6621ab39952fcaa0e5559afe40de99
Bhargav <vyasb1503@gmail.com>
2025-04-19 23:33:31 +0530

    this is the implementation of the controller

--- src/main/java/com/tka/controller/ApplicationController.java
+++ src/main/java/com/tka/controller/ApplicationController.java
@@ -1,5 +1,11 @@
 package com.tka.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tka.ServiceImpl.ApplicationServiceImpl;
+
 public class ApplicationController {
 
+	@Autowired 
+	private ApplicationServiceImpl  applicationServiceImpl; 
 }
