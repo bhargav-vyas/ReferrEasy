c036a51f6dee75df97750fcaf26516e9643ee3ed
Bhargav <vyasb1503@gmail.com>
2025-04-19 23:34:39 +0530

    this is the implementation of the servicelmp

--- src/main/java/com/tka/ServiceImpl/ApplicationServiceImpl.java
+++ src/main/java/com/tka/ServiceImpl/ApplicationServiceImpl.java
@@ -1,5 +1,11 @@
 package com.tka.ServiceImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tka.repository.ApplicationRepository;
+
 public class ApplicationServiceImpl {
+	@Autowired
+	private ApplicationRepository applicationRepository;
 
 }
