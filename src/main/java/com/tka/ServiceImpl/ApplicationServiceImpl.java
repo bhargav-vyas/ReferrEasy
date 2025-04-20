3540c0f69adbf5a29f0ceea63e3632b86e8b313d
Bhargav <vyasb1503@gmail.com>
2025-04-20 18:08:59 +0530

    this  is the implementation of the api of the post the application by the id

--- src/main/java/com/tka/ServiceImpl/ApplicationServiceImpl.java
+++ src/main/java/com/tka/ServiceImpl/ApplicationServiceImpl.java
@@ -2,10 +2,15 @@ package com.tka.ServiceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.tka.entity.Application;
 import com.tka.repository.ApplicationRepository;
 
 public class ApplicationServiceImpl {
 	@Autowired
 	private ApplicationRepository applicationRepository;
+
+	public Application applyToJob(Long userId, Long jobId) {
+		return null;
+	}
 
 }
--- src/main/java/com/tka/controller/ApplicationController.java
+++ src/main/java/com/tka/controller/ApplicationController.java
@@ -1,11 +1,27 @@
 package com.tka.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.tka.ServiceImpl.ApplicationServiceImpl;
-
+import com.tka.entity.Application;
+@RestController
+@RequestMapping("/api/applications")
 public class ApplicationController {
 
 	@Autowired 
 	private ApplicationServiceImpl  applicationServiceImpl; 
+	
+	@PostMapping("/apply")
+	public  Application applyToJob(@RequestParam Long userId , @RequestParam Long jobId) {
+		return applicationServiceImpl.applyToJob(userId,jobId);
+		
+	}
+	
+	
+	
+	
 }
