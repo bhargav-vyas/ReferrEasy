f25e51490489d1868b350600fa1e2de531634004
Bhargav <vyasb1503@gmail.com>
2025-04-20 18:49:22 +0530

    this  is the implementation of the api of the post the application by the id

--- src/main/java/com/tka/ServiceImpl/ApplicationServiceImpl.java
+++ src/main/java/com/tka/ServiceImpl/ApplicationServiceImpl.java
@@ -3,6 +3,7 @@ package com.tka.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.tka.entity.Application;
+import com.tka.entity.User;
 import com.tka.repository.ApplicationRepository;
 import com.tka.repository.Jobrepository;
 import com.tka.repository.UserRepository;
@@ -17,8 +18,8 @@ public class ApplicationServiceImpl {
 	private Jobrepository jobrepository;
 	
 	
-	public Application applyToJob(Long userId, Long jobId) {
-		return null;
+	public Application applyToJob(Long userId, Long jobId) { 
+		User user = userRepository.findById(userId).orElseThrow(()-> new RuntimeException("")
 	}
 
 }
