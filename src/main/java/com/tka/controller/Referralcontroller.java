218d050e9d0105d4f9ca69d7fa61ffe123df1bb7
Bhargav <vyasb1503@gmail.com>
2025-04-24 20:48:38 +0530

    this is the  api of the findReferredEmail in the service Imp

--- src/main/java/com/tka/controller/Referralcontroller.java
+++ src/main/java/com/tka/controller/Referralcontroller.java
@@ -34,10 +34,10 @@ public class Referralcontroller {
 	public Referral getReferralById(@PathVariable Long id) {
 		return referralServiceImpl.getReferraById(id);
 	}
-	@GetMapping("/my-referrals")
-	public List<Referral> getMyReferrals(@RequestParam String email){
-		return referralServiceImpl.findReferralsByEmail(email);
-		
-	}
-}
+//	@GetMapping("/my-referrals")
+//	public List<Referral> getMyReferrals(@RequestParam String email){
+//		return referralServiceImpl.findReferralsByEmail(email);
+//		
+//	}
+//}
   
