472b130c228039ed2b4e40f8b6f696d01b48a100
Bhargav <vyasb1503@gmail.com>
2025-05-02 20:23:05 +0530

    this  corssorigin implementation

--- src/main/java/com/tka/controller/usercontroller.java
+++ src/main/java/com/tka/controller/usercontroller.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.tka.entity.User;
 import com.tka.service.UserService;
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin(origins = "http://localhost:3000")
 public class usercontroller {
 	@Autowired 
 	private UserServiceImpl userServiceImpl;
