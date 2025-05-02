3e729f46e57d21e9648a57ea2beb023f4b62739c
Bhargav <vyasb1503@gmail.com>
2025-05-02 20:21:13 +0530

    this is the connection between the  front end and backend

---
+++ src/main/java/com/tka/Security/WebConfig.java
@@ -0,0 +1,15 @@
+package com.tka.Security;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+	  @Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**")  // Allow all endpoints
+	                .allowedOrigins("http://localhost:3000")  // Your React app
+	                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+	                .allowedHeaders("*")
+	                .allowCredentials(true);
+	  }
+}
